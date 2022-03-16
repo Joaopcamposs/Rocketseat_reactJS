70fc7a4a5203b1e56029603b943facb03c6e8faa
Joaopcamposs <joaopcamposs@outlook.com>
2022-03-16 13:50:39 -0300

    Andamento dos componentes tweets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import { Tweet } from "./components/Tweet"
 
 function App() {
-  return <Tweet/>
+  const [tweets, setTweets] = useState<string[]>([
+    'Tweet 1',
+    'Tweet 2',
+    'Tweet 3',
+    'Tweet 4',
+  ]);
+
+  function createTweet(){
+    setTweets([...tweets, 'Tweet 5'])
+  }
+
+  return (
+    <div>
+      {
+        tweets.map(tweet => {
+          return <Tweet text={tweet}s/>
+        })
+      }
+
+      <button onClick={createTweet}>Adicionar Tweet</button>
+    </div>
+  );
 }
 
 export default App
